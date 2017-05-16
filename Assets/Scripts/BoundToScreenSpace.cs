1a0017e654c324529fa354fbcf76692384cf176e
Koen Sparreboom <koensparreboom@gmail.com>
2017-05-16 11:05:25 +0200

    Added bounds script

---
+++ Assets/Scripts/BoundToScreenSpace.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundToScreenSpace : MonoBehaviour {
+
+    [SerializeField]
+    float offset = 2.5f;
+
+    float left;
+    float right;
+
+    Vector2 horizontal;
+
+    float top;
+    float bottom;
+
+    Vector2 vertical;
+
+	void Update () {
+        horizontal = new Vector2(
+            Camera.main.ScreenToWorldPoint(Vector3.zero).x - offset,
+            Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0)).x + offset
+        );
+
+        vertical = new Vector2(
+            Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height)).y + offset,
+            Camera.main.ScreenToWorldPoint(new Vector3(0, 0)).y - offset
+        );
+
+        // horizontal
+        if (transform.position.x <= horizontal.x)
+        {
+            transform.position = new Vector3(horizontal.y, transform.position.y);
+        }
+        else if (transform.position.x >= horizontal.y)
+        {
+            transform.position = new Vector3(horizontal.x, transform.position.y);
+        }
+
+        // vertical
+        if (transform.position.y >= vertical.x)
+        {
+            transform.position = new Vector3(transform.position.x, vertical.y);
+        }
+        else if (transform.position.y <= vertical.y)
+        {
+            transform.position = new Vector3(transform.position.x, vertical.x);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
 
+    PlayerManager playerManager;
 	PlayerMovement playerMovement;
     PlayerShoot playerShoot;
 
-    [SerializeField]
-    bool lookAtCursor;
-
 	void Start () {
+        playerManager = GetComponent<PlayerManager>();
 		playerMovement = GetComponent<PlayerMovement>();
         playerShoot = GetComponent<PlayerShoot>();
 	}
 
-	void Update () {
-		//playerMovement.LookAtCursor(Input.mousePosition);
-
-		playerMovement.Move(Input.GetAxis("PlayerOneHorizontal"), Input.GetAxis("PlayerOneVertical"), Input.GetKey(KeyCode.Space));
-
+    void Update() {
+        //playerMovement.LookAtCursor(Input.mousePosition);
+        switch (playerManager.player)
+        {
+            case PlayerManager.Player.One:
+                playerMovement.Move(Input.GetAxis("PlayerOneHorizontal"), Input.GetAxis("PlayerOneVertical"), Input.GetKey(KeyCode.Space));
+                break;
+            case PlayerManager.Player.Two:
+                playerMovement.Move(Input.GetAxis("PlayerTwoHorizontal"), Input.GetAxis("PlayerTwoVertical"), Input.GetKey(KeyCode.Keypad0));
+                break;
+        }
         if (Input.GetKeyDown(KeyCode.Q))
         {
             playerShoot.Shoot();
