1f43d21eed3f1259dbc6e609d522119249fdc30f
Koen Sparreboom <koensparreboom@gmail.com>
2017-05-23 10:55:25 +0200

    Added MainMenu scene

--- Assets/Scripts/Utility/Background.cs
+++ Assets/Scripts/Utility/Background.cs
@@ -1,28 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class Background : MonoBehaviour
 {
     [SerializeField]
-    bool randomizeBackground = true;
-    [SerializeField]
-    Sprite[] randomWallpaper;
-
+    Sprite[] backgrounds;
     [SerializeField]
     bool scaleBackground = true;
+
+    [SerializeField]
+    Image previewImage;
+
+    int currentBackground = 0;
 
     SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-
-        if (randomizeBackground)
-        {
-            spriteRenderer.sprite = randomWallpaper[Random.Range(0, randomWallpaper.Length)];
-        }
 
         if (scaleBackground)
         {
@@ -37,5 +35,31 @@ public class Background : MonoBehaviour
 
             transform.localScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, 0);
         }
+    }
+
+    public void NextBackground()
+    {
+        if (currentBackground >= backgrounds.Length - 1)
+            currentBackground = 0;
+        else
+            currentBackground++;
+
+        ChangeBackground();
+    }
+
+    public void PreviousBackground()
+    {
+        if (currentBackground <= 0)
+            currentBackground = backgrounds.Length - 1;
+        else
+            currentBackground--;
+
+        ChangeBackground();
+    }
+
+    void ChangeBackground()
+    {
+        spriteRenderer.sprite = backgrounds[currentBackground];
+        previewImage.sprite = backgrounds[currentBackground];
     }
 }
