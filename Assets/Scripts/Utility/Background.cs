3f2a3fd4e01692e3a5c3dd5482cc68169db48494
Koen Sparreboom <koensparreboom@gmail.com>
2017-05-23 15:08:20 +0200

    Added ability to flip the background

--- Assets/Scripts/Utility/Background.cs
+++ Assets/Scripts/Utility/Background.cs
@@ -99,6 +99,12 @@ public class Background : MonoBehaviour
         ChangeBackground();
     }
 
+    public void FlipBackground()
+    {
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+        //spriteRenderer.flipX = !spriteRenderer.flipX;
+    }
+
     void ChangeBackground()
     {
         previewImage.sprite = backgrounds[backgroundIndex];
