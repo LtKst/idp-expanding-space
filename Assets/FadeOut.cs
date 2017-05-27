4e67c143e0dc8c38e132fd3b85a61f3f96cb670c
Koen Sparreboom <koensparreboom@gmail.com>
2017-05-27 22:27:23 +0200

    Quitting to the menu now works

---
+++ Assets/FadeOut.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour {
+
+    float lerpSpeed = 4;
+
+    Color currentColor;
+
+    bool fadedOut = false;
+
+    private void Start()
+    {
+        currentColor = GetComponent<Image>().color;
+    }
+
+    private void Update()
+    {
+        if (!fadedOut)
+        {
+            GetComponent<Image>().color = Color.Lerp(GetComponent<Image>().color, new Color(currentColor.r, currentColor.g, currentColor.b, 0), Time.unscaledDeltaTime * lerpSpeed);
+
+            if (GetComponent<Image>().color.a <= 0.007)
+                fadedOut = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/LoadLevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelManager : MonoBehaviour {
+
+    [SerializeField]
+    Image fadeImage;
+    [SerializeField]
+    float fadeSpeed = 5;
+
+    bool loadingLevel;
+
+    private void Update()
+    {
+        if (loadingLevel)
+        {
+            fadeImage.color = Color.Lerp(fadeImage.color, Color.black, Time.unscaledDeltaTime * fadeSpeed);
+            AudioListener.volume = Mathf.Lerp(AudioListener.volume, 0, Time.unscaledDeltaTime * fadeSpeed * 2);
+
+            if (fadeImage.color.a >= 0.95f)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    public void LoadScene()
+    {
+        loadingLevel = true;
+    }
+
+    public bool IsLoading
+    {
+        get
+        {
+            return loadingLevel;
+        }
+    }
+}
