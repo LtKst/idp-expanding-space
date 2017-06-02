504312adaa4511b1f8d18f8e905a52cddd73ff26
Koen Sparreboom <koensparreboom@gmail.com>
2017-06-02 14:11:58 +0200

    Added FrameAnimation.cs

---
+++ Assets/FrameAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrameAnimation : MonoBehaviour {
+
+    [SerializeField]
+    Sprite[] frames;
+    [SerializeField]
+    float fps = 24;
+
+    Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        int index = (int)((Time.time * fps) % frames.Length);
+
+        image.sprite = frames[index];
+        
+    }
+}
